1024090f22a640d50cf542bde166a81d6dcfee53
Bartosz Zasieczny <barteqx@gmail.com>
2013-11-17 21:40:27 +0100

    more and more stuff

--- prog/functions.h
+++ prog/functions.h
@@ -17,9 +17,9 @@ double func_2_d_2 (double x) { return exp(pow((-1)*x, 2))*(4*pow(x, 2) - 2); }
 //Third function
 double func_3 (double x) { return x/(1+pow(x, 2)); }
 
-double func_3_d (double x) { return 2*x*(pow(x, 2)-3)/pow(pow(x, 2) + 1, 3); }
+double func_3_d (double x) { return (1 - pow(x,2))/pow(pow(x, 2) + 1, 2);}
 
-double func_3_d_2 (double x) { return 6*(pow(x,4) - 6*pow(x, 2) + 1)/pow(pow(x, 2) + 1, 4); }
+double func_3_d_2 (double x) { return 2*x*(pow(x, 2)-3)/pow(pow(x, 2) + 1, 3); }
 
 //Fourth function
 double func_4 (double x) { return pow(x,2) + x - 1; }
